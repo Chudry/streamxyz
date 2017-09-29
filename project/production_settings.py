7e43217ec41d000f96c19a46b0a3cbf4d0fa8ebb
Ravi Simon <simonravi@yahoo.com>
2017-09-29 15:47:46 +0530

    added allowed hosts

--- project/production_settings.py
+++ project/production_settings.py
@@ -2,6 +2,8 @@ from .base_settings import *
 
 import dj_database_url
 
+DEBUG = False
+ALLOWED_HOSTS = ["streamxyz.herokuapp.com", "www.streamxyz.com", "streamxyz.com"]
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
